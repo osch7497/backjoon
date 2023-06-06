505b18c1507f3fb0d8b4f72351d17c3f279f2721
osch7497 <73733941+osch7497@users.noreply.github.com>
2023-06-06 11:01:37 +0900

    Create 2750.cpp

---
+++ 2750.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int vs[1000];
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i = 0; i < a; i++)
+        scanf("%d",&vs[i]);
+    sort(vs,vs+a);
+    for(int i = 0; i < a; i++)
+        printf("%d\n",vs[i]);
+}
