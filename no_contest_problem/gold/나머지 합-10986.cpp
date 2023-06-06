b386ceae45c0dabee78cb6a2e3e52885460cdb7f
osch7497 <73733941+osch7497@users.noreply.github.com>
2023-06-06 12:50:26 +0900

    Save 나머지 합-10986.cpp

--- no_contest_problem/gold/나머지 합-10986.cpp
+++ no_contest_problem/gold/나머지 합-10986.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+#include <algorithm>
+using namespace std;
+long long input[10000000],tree[40000000];
+int set_segment(int start,int end,int node){
+    if(start==end)return tree[node] = input[start];
+    int mid=(start+end)/2;
+}
+int main(){
+    
+    for(int i = 0; i < )
+}
