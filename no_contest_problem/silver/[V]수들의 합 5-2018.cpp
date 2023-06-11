c247779e199c893856b61dff12fb9550758265fb
osch7497 <73733941+osch7497@users.noreply.github.com>
2023-06-11 11:59:59 +0900

    Update [V]수들의 합 5-2018.cpp
    Unsolved

--- no_contest_problem/silver/[V]수들의 합 5-2018.cpp
+++ no_contest_problem/silver/[V]수들의 합 5-2018.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+int N,ds;
+int DP[5000001]{0,};
+int display(int x,int y){
+    for(int i = 1; i <= N/2;i++)
+        printf("[%3d]",DP[i]);
+    cout<<endl;
+    for(int i = 1; i <= N/2;i++)
+        printf("%s",(i == x||i == y)?"  ^  ":"     ");
+    cout<<endl;
+}
+int main()
+{
+    scanf("%d",&N);
+    int i = 0;
+    for(i = 1; i <= N/2;i++){
+        DP[i] = i+DP[i-1];
+    }
+    for(i = 1; i <= N/2;i++){
+        printf("[%3d]",DP[i]);
+    }cout<<endl;
+    for(int i = 1; i <= N;i++)for(int n = i; n >= 0; n--){
+        if(DP[i]-DP[n] == N){ds++;break;}
+    }
+    printf("%d\n",ds+1);
+}
